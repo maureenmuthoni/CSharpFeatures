856b995d374201efc373f5c17df776e5efc13336
maureenmuthoni <muthonimut@gmail.com>
2017-08-22 16:06:39 +1000

    Ball Changes

---
+++ Assets/!Globals/Scripts/Debbuging/VisualisedBound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Renderer))]
+
+public class VisualisedBound : MonoBehaviour
+{
+  private Renderer rend;
+
+    void onDrawGizmos()
+    {
+        rend = GetComponent<Renderer>();
+        Gizmos.DrawWireCube(transform.position, rend.bounds.size);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
