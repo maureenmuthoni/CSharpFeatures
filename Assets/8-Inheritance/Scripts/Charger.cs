ae92292a082b97b1562e7f3a5310e3884c26b665
maureenmuthoni <muthonimut@gmail.com>
2017-10-10 16:07:25 +1100

    Inheritance  uploads

---
+++ Assets/8-Inheritance/Scripts/Charger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inheritance
+{
+
+
+    public class Charger :Enemy
+    {
+        [Header("Charger")]
+        public float impactForce = 10f;
+        public float knockback = 5f;
+
+        public override void Attack()
+        {
+            // Add force to self
+        }
+        void OnCollisionEnter(Collision col)
+        {
+            // if collision hits player
+            // Add impactForce to player
+        }
+
+
+
+    }
+}
---
+++ Assets/8-Inheritance/Scripts/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Inheritance
+{
+   
+  
+    public class Enemy : MonoBehaviour
+    {
+        [Header("Enemy")]
+        public Transform target;
+        public int health = 100;
+        public int damage = 20;
+        public float attackRange = 2f; 
+        public float attackRate = 0.5f;
+
+        private float attackTimer = 0f;
+        private NavMeshAgent nav;
+        private Rigidbody rigid;
+        void Awake()
+        {
+            nav = GetComponent<NavMeshAgent>();
+            rigid = GetComponent<Rigidbody>();
+                }
+
+
+        void Update()
+        {
+            //Increase attack Timer
+            attackTimer += Time.deltaTime;
+            //Get distance from enemy to target
+            float distance = Vector3.Distance(transform.position, target.position);
+            //If distance < atttack range
+            if(distance < attackRange)
+            {
+                // Call Attack()
+                Attack();
+                // Reset attack timer
+                attackTimer = 0f;
+
+            }
+            if (target != null)
+            {
+                // Navigate to target
+                nav.SetDestination(target.position);
+            }
+        }
+      public virtual  void Attack()
+        {
+            print("Ya dun goofed");
+        }
+
+     
+    }
+}
